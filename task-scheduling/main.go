527747df594fd4de5163a1990037c87052b04e3b
T2Knock <huynhtransongngoc@gmail.com>
2026-04-02 11:14:53 +0700

    feat: task scheduling min total time

--- task-scheduling/main.go
+++ task-scheduling/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"container/list"
+	"errors"
 	"fmt"
+	"log"
 	"math/rand"
 	"slices"
 )
@@ -13,42 +15,64 @@ type Task struct {
 	durationMs   int
 }
 
-func topSort(tasks []Task) []string {
+// minTotalTime using top sort Kahn algorithms
+func minTotalTime(tasks []Task) (int, error) {
 	inDegree := make(map[string]int)
-	adjList := make(map[string][]string)
-	queue := list.New()
-	sortedList := []string{}
+	adjList := make(map[string][]Task)
 
 	for _, task := range tasks {
 		inDegree[task.id] = len(task.dependencies)
 
 		for _, dep := range task.dependencies {
-			adjList[dep] = append(adjList[dep], task.id)
+			adjList[dep] = append(adjList[dep], task)
 		}
 	}
+
+	queue := list.New()
+	earliestFinish := make(map[string]int)
 
 	for _, task := range tasks {
 		if inDegree[task.id] == 0 {
-			queue.PushBack(task.id)
+			queue.PushBack(task)
+			earliestFinish[task.id] = task.durationMs
 		}
 	}
 
+	sortedList := []string{}
+
 	for queue.Len() > 0 {
 		e := queue.Front()
-		u := e.Value.(string)
+		u := e.Value.(Task)
 		queue.Remove(e)
 
-		sortedList = append(sortedList, u)
+		sortedList = append(sortedList, u.id)
+		uFinishTime := earliestFinish[u.id]
 
-		for _, neighbor := range adjList[u] {
-			inDegree[neighbor] -= 1
-			if inDegree[neighbor] == 0 {
+		for _, neighbor := range adjList[u.id] {
+			newFinishTime := uFinishTime + neighbor.durationMs
+			if newFinishTime > earliestFinish[neighbor.id] {
+				earliestFinish[neighbor.id] = newFinishTime
+			}
+
+			inDegree[neighbor.id]--
+			if inDegree[neighbor.id] == 0 {
 				queue.PushBack(neighbor)
 			}
 		}
 	}
 
-	return sortedList
+	if len(sortedList) != len(tasks) {
+		return 0, errors.New("cycle detected")
+	}
+
+	maxTime := 0
+	for _, t := range earliestFinish {
+		if t > maxTime {
+			maxTime = t
+		}
+	}
+
+	return maxTime, nil
 }
 
 func generateTask(n int) []Task {
@@ -83,10 +107,13 @@ func main() {
 	tasks := generateTask(10)
 
 	for _, t := range tasks {
-		fmt.Printf("ID: %s, Dur: %d, Deps: %v \n", t.id, t.durationMs, t.dependencies)
+		fmt.Printf("ID: %s, Dur: %d ms, Deps: %v \n", t.id, t.durationMs, t.dependencies)
 	}
 
-	sortedList := topSort(tasks)
+	minTotal, err := minTotalTime(tasks)
+	if err != nil {
+		log.Println(err)
+	}
 
-	fmt.Println(sortedList)
+	fmt.Printf("Minimum total time for task scheduling = %d ms", minTotal)
 }
